f64d34d0700d7a6bab96fa26582dee4f54fb960f
9randeza <geydarb0412@gmail.com>
2023-12-17 14:51:32 +0400

    done version with try{} catch{}

--- dateClass.cpp
+++ dateClass.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+struct WrongDay{
+	int day;
+};
+
+struct WrongMonth{
+	int month;
+};
+
+struct WrongYear{
+	int year;
+};
+
 class Date{
 	private:
 		int year;
@@ -9,135 +21,75 @@ class Date{
 		bool isLeap(int year) {
 			return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0); // That's what Caesar said
 		}
-		
-		void Normalize(){
-			while (month > 12) {
-            month -= 12;
-            year++;
-			}
-
-			while (month < 1) {
-            month += 12;
-            year--;
-			}
-			
-			while (day < 1) {
-				month--;
-				if (month < 1) {
-					month += 12;
-					year--;
-				}
-			
-				if(month == 2){
-					if(isLeap(year)){
-						day += 29;
-					}else{
-						day += 28;
-					}
-				}
-			
-				if (month == 4 || month == 6 || month == 9 || month == 11){
-					day += 30;
-				}
-				if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
-					day += 31;
-				}
-			}
-			
-			int daysInMonth = 31;
-			
-			if (month == 4 || month == 6 || month == 9 || month == 11){
-				daysInMonth = 30;
-			}
-			if(month == 2){
-				if(isLeap(year)){
-					daysInMonth = 29;
-				}else{
-					daysInMonth = 28;
-				}
-			}
-			
-			while (day > daysInMonth) {
-				day -= daysInMonth;
-				month++;
-				if (month > 12) {
-					month -= 12;
-					year++;
-				}
-				daysInMonth = 31;
-			
-				if (month == 4 || month == 6 || month == 9 || month == 11){
-					daysInMonth = 30;
-				}
-				if(month == 2){
-					if(isLeap(year)){
-						daysInMonth = 29;
-					}else{
-						daysInMonth = 28;
-					}
-				}
-			}
-		}
-		
 	public:
-		Date(int y, int m, int d);
-		Date(int d);
-		int GetYear()const;
-		int GetMonth()const;
-		int GetDay()const;
-		
-		   Date operator+ (int d) const {
-			int newDay = day + d;
-			return Date(newDay, month, year);
+		Date(int y, int m, int d)
+		{
+			if(y < 1){
+				throw WrongYear(y);
 			}
-
-		Date operator- (int d) const{
-			int newDay = day - d;
-			return Date(newDay, month, year);
-		}
-
-		Date& operator+= (int d){
-			day += d;
 			
-			Normalize();
-
-        return *this;
+			if(m < 1 || m > 12){
+				throw WrongMonth(m);
+			}
+			
+			if(isLeap(y)){
+				if(m == 2){
+					if(d > 29 || d < 1){
+						throw WrongDay(d);
+					}
+				}
+			}else if(m == 2){
+				if(d > 28 || d < 1){
+					throw WrongDay(d);
+				}
+			}
+			if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12){
+				if(d < 1 || d > 31){
+					throw WrongDay(d);
+				}
+			}
+			if(m == 4 || m == 6 || m == 9 || m == 11){
+				if(d < 1 || d > 30){
+					throw WrongDay(d);
+				}
+			}
 		}
+		
 };
-
-int Date::GetYear()const{
-	return year;
-}
-
-int Date::GetMonth()const{
-	return month;
-}
-
-int Date::GetDay()const{
-	return day;
-}
-
-Date::Date(int y, int m, int d){
-	 year = y;
-     month = m;
-     day = d;
-     Normalize();
-}
-
-Date::Date(int d) {
-    year = 0;
-    month = 1;
-    day = d;
-    Normalize();
-}
 
 int main()
 {
-	Date d( 5343);
-	std::cout << d.GetYear() << " ";
-	std::cout << d.GetMonth() << " ";
-	std::cout << d.GetDay() << std::endl;
-	
+	try{
+	Date d(-12, 5, 23);
+	 }
+
+    catch (const WrongDay& d) {
+        std::cerr << "Wrong day" << std::endl;
+        return 1;
+    }
+
+    catch (const WrongMonth& m) {
+        std::cerr << "Wrong month" << std::endl;
+        return 1;
+    }
+
+    catch (const WrongYear& y) {
+        std::cerr << "Wrong year" << std::endl;
+        return 1;
+    }
+
+    catch (const std::istream::failure& ex)
+    {
+        std::cerr << "Data could not be read " << ex.what() << "\n";
+        return 2;
+    }
+
+    catch (...)
+    {
+        std::cerr << "other failure " << "\n";
+        return -1;
+    }
+    
 	return 0;
 	
 }
